a1540b9626ffe877994f31e358f6774baec85aea
gk1227 <girishkumar.mailbox@gmail.com>
2020-12-13 02:06:15 -0500

    Installed mongodb and connected it to the shopping application. Created a database for the application.
    Signed-off-by: gk1227 <girishkumar.mailbox@gmail.com>

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var expressejs = require('express-handlebars');
+var mongoose = require('mongoose');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+mongoose.connect('localhost:27017/shopping');
 // view engine setup
 app.engine('.ejs',expressejs({defaultLayout: 'layout', extname: '.ejs'}));
 app.set('views', path.join(__dirname, 'views'));
